7aae45ac65c4312accb01bfec9ba5c59a90df251
Miroslav <gavrilov.miroslav@gmail.com>
2021-03-25 12:23:04 +0100

    Update mgavrilov.h

--- mgavrilov.h
+++ mgavrilov.h
@@ -83,3 +83,13 @@ std::string mgavrilov__vowelChange_test_hotPaths(std::string k)
 
     return k;
 }
+
+std::string mgavrilov__vowelChange_test_stdlib(std::string k)
+{
+    std::replace_if(k.begin(), k.end(), [](char c) { return c == 'a' || c == 'A'; }, '1');
+    std::replace_if(k.begin(), k.end(), [](char c) { return c == 'e' || c == 'E'; }, '2');
+    std::replace_if(k.begin(), k.end(), [](char c) { return c == 'i' || c == 'I'; }, '3');
+    std::replace_if(k.begin(), k.end(), [](char c) { return c == 'o' || c == 'O'; }, '4');
+    std::replace_if(k.begin(), k.end(), [](char c) { return c == 'u' || c == 'U'; }, '5');
+    return k;
+}
